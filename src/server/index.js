26b5823ff6ce43eb60b4a2f681aaf7675ab270ac
Llausa <santiago.chamon@gmail.com>
2020-01-23 10:17:50 +1000

    ADD moar CRUD to backend

--- src/server/index.js
+++ src/server/index.js
@@ -27,7 +27,7 @@ app.get("/api/lookaheads", (req, res) => {
 // Lookahead Project view route
 app.get("/api/lookaheads/:id", (req, res) => {
   const lookahead = lookaheads.find(c => c.id === parseInt(req.params.id))
-  if (!lookahead) res.status(404).send('The project with that ID was not found')
+  if (!lookahead) return res.status(404).send('The project with that ID was not found')
   res.send(lookahead)
 })
 
@@ -39,11 +39,7 @@ app.post("/api/lookaheads", (req, res) => {
 
   const result = Joi.validate(req.body, schema)
 
-  if (result.error) {
-    // 400 Bad Request
-    res.status(400).send(result.error.details[0].message)
-    return
-  }
+  if (result.error) return res.status(400).send(result.error.details[0].message)
   const lookahead = {
     id: lookaheads.length + 1,
     name: req.body.name
@@ -55,7 +51,7 @@ app.post("/api/lookaheads", (req, res) => {
 // Lookahead Project PUT route
 app.put('/api/lookaheads/:id', (req, res) => {
   const lookahead = lookaheads.find(c => c.id === parseInt(req.params.id))
-  if (!lookahead) res.status(404).send('The project with that ID was not found')
+  if (!lookahead) return res.status(404).send('The project with that ID was not found')
 
   const schema = {
     name: Joi.string().min(3).required()
@@ -67,6 +63,16 @@ app.put('/api/lookaheads/:id', (req, res) => {
     res.status(400).send(result.error.details[0].message)
     return
   }
+  res.send(lookahead)
+})
+
+app.delete('/api/lookaheads/:id', (req, res) => {
+  const lookahead = lookaheads.find(c => c.id === parseInt(req.params.id))
+  if (!lookahead) return res.status(404).send('The project with that ID was not found')
+
+  const index = lookaheads.indexOf(lookahead)
+  lookaheads.splice(index, 1)
+
   res.send(lookahead)
 })
 
