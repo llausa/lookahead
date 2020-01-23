932764a3da05c45fcea02ef74a4354790197a34f
Ben Ulcoq <ben.ulcoq@gmail.com>
2020-01-24 09:09:24 +1000

    Schemas

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -1,15 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-// Create a simple User's schema 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     firstName: { type: String, required: true},
     lastName: {type: String, required: true},
     email: { type: String, required: true},
     password: { type: String, required: true},
     position: { type: String, default: "worker"}
-});
+})
 
-const userModel = new mongoose.model('User', userSchema);
+const UserModel = new mongoose.model('User', UserSchema)
 
-
-module.exports = userModel
+module.exports = UserModel
