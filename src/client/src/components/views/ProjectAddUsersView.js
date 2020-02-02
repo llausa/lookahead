91aae38c597afa2d7038567c6197d785512b2016
alexleybourne <alexleybourne1@gmail.com>
2020-02-02 21:41:23 +1000

    CSS means Constantly Stressed Styling

--- src/client/src/components/views/ProjectAddUsersView.js
+++ src/client/src/components/views/ProjectAddUsersView.js
@@ -53,8 +53,8 @@ const ProjectAddUsersView = () => {
         <div data-cy="projectsView" style={mystyle}>
         <TitleText text="Project Add Users" style={{ marginTop: "30px"}} />
 
-        <UserCard user="Yuri Tarded" userPrivilege="none" userEmail="MrYeet@yeetmail.com" />
-        <UserCard user="GONK MAN" userPrivilege="none" userEmail="MrYeet@yeetmail.com" />
+        <UserCard user="Marty McFly" userPrivilege="none" userEmail="Back2The@Future.com" />
+        <UserCard user="Keanu Reeves" userPrivilege="none" userEmail="DontHurtMyDog@gmail.com" />
         <UserCard/>
 
         </div>
