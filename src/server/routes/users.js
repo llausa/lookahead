e3c8779574d20cde6ba1d9a7ed477d3c1594350e
Llausa <santiago.chamon@gmail.com>
2020-02-02 20:58:44 +1000

    ADD comments on api endpoints

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -7,16 +7,16 @@ const userController = require('../controllers/userController')
 const { UserModel, validateUser } = require('../models/user')
 
 // Get current user details
-router.get('/details', auth, userController.details)
+router.get('/details', auth, userController.details) // works
 
 // Register User
-router.post('/', userController.register)
+router.post('/', userController.register) // works
 
 // Update User details
-router.put('/details', auth, userController.updateDetails)
+router.put('/details', auth, userController.updateDetails) // not validating body of request atm giving error on console
 // Update User password
-router.put('/password', auth, userController.updatePassword)
+router.put('/password', auth, userController.updatePassword) // see slack - we're not handling body with wrong data correctly
 // Update User email
-router.put('/email', auth, userController.updateEmail)
+router.put('/email', auth, userController.updateEmail) // works
 
 module.exports = router
