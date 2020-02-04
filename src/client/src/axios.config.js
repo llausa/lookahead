39ea2595944bcde328b083921511ba6aab9be7ee
Llausa <santiago.chamon@gmail.com>
2020-02-04 18:21:15 +1000

    Axios pointing to prod api

---
+++ src/client/src/axios.config.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+
+
+const API = axios.create({
+    // baseURL: "http://localhost:3001/"
+    baseURL: "https://vast-oasis-18718.herokuapp.com/"
+})
+
+let token = localStorage.getItem('authToken')
+
+if (token) {
+  API.defaults.headers.common['Authorization'] = `Bearer ${token}`
+}
+
+export default API
