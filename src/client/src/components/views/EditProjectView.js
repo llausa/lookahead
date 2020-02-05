4cf15f4e7b8cdf26beba766c4745bbd22f790388
Llausa <santiago.chamon@gmail.com>
2020-02-05 22:38:55 +1000

    ADD Project edit link

--- src/client/src/components/views/EditProjectView.js
+++ src/client/src/components/views/EditProjectView.js
@@ -10,41 +10,44 @@ import NormalText from '../NormalText'
 import TimeZonePicker from '../TimeZonePicker'
 import FormInput from '../FormInput'
 import Button from '@material-ui/core/Button'
+import { useParams } from 'react-router-dom'
 
 
 const EditProjectView = () => {
 
+
     const [data, setData] = useReducer((state, newState) => (
         {...state, ...newState}
       ), {
-        projectTitle: '',
+        title: '',
         location: '',
-        startDate: '',
-        endDate: ''
+        start_date: '',
+        end_date: '',
+        create_date: ''
       })
-    
+
       const onSubmit = e => {
         e.preventDefault()
-    
+
         console.log(data)
-    
+
         API.post(
         '/api/users', data)
         .then(function (response) {
-            
+
             console.log(response)
         })
         .catch(function (error) {
             console.log(error.response.data)
         })
-        
-      }
-    
+
+        }
+
       const onChange = e => {
         setData({[e.target.name]: e.target.value})
-        console.log(data) 
+        console.log(data)
         // setData({"location": location.value})
-    }
+      }
 
     const mystyle = {
         display: "flex",
@@ -64,8 +67,10 @@ const EditProjectView = () => {
       fontSize: "10px",
       width: "80%",
       alignSelf: "center"
-  }
-    
+    }
+
+    const { projectId } = useParams()
+
     // Client Validation
     const basic = (text) => text.length > 2
 
@@ -73,22 +78,22 @@ const EditProjectView = () => {
 
     return (
         <>
-      <Nav backButtonLink = "/projects" BackButton={true} MenuButton={false} />
-      <CardContainer background={Background}>
-      <form onSubmit={onSubmit} className='form'>
-        <div data-cy="newProjectView" style={mystyle}>
-            <TitleText text="Edit Project" />
-            <NormalText text="Please fill out all required fields" />
-            <FormInput type='text' validation={basic} value={data.projectTitle} onChange={onChange} require={true} errorText="Please enter more Characters" label='Project Title' id='projectTitle' name='projectTitle'/>
-            <TimeZonePicker label="Location*" id="location" name='location'/>
-            <p>Start Date: {startDate}</p>
-            <DateInput label="End Date" day={2} id="endDate" name='endDate' />
-            <ButtonInput disabled={false} type='submit' primary={true} color='primary' text="Save" />
-            <Button variant="outlined" style={buttonResetP}>Delete Project</Button>
-        </div>
-        </form>
-        </CardContainer>
-        <Background/>
+          <Nav backButtonLink = {`/projects/${projectId}`} BackButton={true} MenuButton={false} />
+          <CardContainer background={Background}>
+          <form onSubmit={onSubmit} className='form'>
+            <div data-cy="newProjectView" style={mystyle}>
+                <TitleText text="Edit Project" />
+                <NormalText text="Please fill out all required fields" />
+                <FormInput type='text' validation={basic} value={data.projectTitle} onChange={onChange} require={true} errorText="Please enter more Characters" label='Project Title' id='projectTitle' name='projectTitle'/>
+                <TimeZonePicker defaultVal={(val) => setData(val)} label="Location*" id="location" name='location'/>
+                <p>Start Date: {startDate}</p>
+                <DateInput label="End Date" day={2} id="endDate" name='endDate' />
+                <ButtonInput disabled={false} type='submit' primary={true} color='primary' text="Save" />
+                <Button variant="outlined" style={buttonResetP}>Delete Project</Button>
+            </div>
+            </form>
+            </CardContainer>
+            <Background/>
         </>
     )
 }
