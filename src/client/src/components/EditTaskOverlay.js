e9fb9fed113c25548dad67fe2639a6afebe63e35
alexleybourne <alexleybourne1@gmail.com>
2020-02-05 22:28:46 +1000

    Overlay Form Created

--- src/client/src/components/EditTaskOverlay.js
+++ src/client/src/components/EditTaskOverlay.js
@@ -1,19 +1,16 @@
 import React, {useReducer} from "react"
-import API from "../../axios.config"
-import CardContainer from '../CardContainer'
-import Nav from '../Nav'
-import DateInput from '../DateInput'
-import ButtonInput from '../ButtonInput'
-import Background from '../Background'
-import TitleText from '../TitleText'
-import NormalText from '../NormalText'
-import DurationPicker from '../DurationPicker'
-import FormInput from '../FormInput'
-
-import EditTaskOverlay from '../EditTaskOverlay'
+import API from "../axios.config"
+import CardContainer from './CardContainer'
+import DateInput from './DateInput'
+import ButtonInput from './ButtonInput'
+import TitleText from './TitleText'
+import NormalText from './NormalText'
+import DurationPicker from './DurationPicker'
+import TimePicker from './TimePicker'
+import FormInput from './FormInput'
 
 
-const NewTaskView = () => {
+const EditTaskOverlay = (props) => {
 
     const [data, setData] = useReducer((state, newState) => (
         {...state, ...newState}
@@ -63,28 +60,29 @@ const NewTaskView = () => {
 
     return (
         <>
-        
-      <Nav backButtonLink = "/projects" BackButton={true} MenuButton={false} />
-      {/* <CardContainer background={Background}>
+      <div style={{position: "absolute", zIndex: "6", height: "100vh", width: "100vw"}}>
+      <CardContainer style={{zIndex: "6"}} >
       <form onSubmit={onSubmit} className='form'>
         <div data-cy="newProjectView" style={mystyle}>
-            <TitleText text="New Task" />
+            <TitleText text={ props.edit? ("Edit Task") : ("New Task")  } />
             <NormalText text="Please fill out all required fields" />
             <FormInput type='text' validation={basic} value={data.taskName} onChange={onChange} require={true} errorText="Please enter more Characters" label='Task Name' id='taskName' name='taskName' />
             <FormInput type='text' validation={basic} value={data.description} onChange={onChange} require={false} multiline={true} label='Task Description' id='description' name='description' />
             
             <DateInput label="Start Date" day={1} id="startDate" name='startDate'/>
             
+            <TimePicker label="Start Time*" id="startTime" name='startTime' style={{width: "100%"}}/>
             <DurationPicker label="Duration (hours)*" id="duration" name='duration' style={{width: "100%"}}/>
 
-            <ButtonInput disabled={false} type='submit' primary={true} color='primary' text="Create" />
+            <ButtonInput disabled={false} type='submit' primary={true} color='primary' text={ props.edit? ("Save") : ("Create") } />
         </div>
         </form>
-        </CardContainer> */}
-        <EditTaskOverlay edit />
-        <Background/>
+        </CardContainer>
+        </div>
+        <div className="blackOverlay" ></div>
         </>
+       
     )
 }
 
-export default NewTaskView
+export default EditTaskOverlay
