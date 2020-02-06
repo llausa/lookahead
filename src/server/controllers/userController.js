f756a8de0cba263336044c33b9774e2c1536bdc1
Llausa <santiago.chamon@gmail.com>
2020-02-06 20:37:39 +1000

    ADD Controller Comments

--- src/server/controllers/userController.js
+++ src/server/controllers/userController.js
@@ -3,7 +3,9 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const _ = require('lodash')
 
+// Get User Details route Logic
 async function details(req, res, next) {
+  // Tries to grab user with req Id and return 404 if no valid user found
   const user = await UserModel.findById(req.user._id).select('-password -email')
   .catch( (err) => { return res.status(404).json({ "message": err.message })})
 
@@ -13,15 +15,16 @@ async function details(req, res, next) {
   next()
 }
 
+// Register User Route Logic
 async function register(req, res, next) {
-
+  // Tries to grab user with req Id and return 404 if no valid user found
   const { error } = validateUser(req.body)
   if (error) return res.status(400).json({"message": 'Invalid user data.'})
-
+  // Grab User Object based on email
   let user = await UserModel.findOne({ email: req.body.email })
-  if(user) return res.status(409).json({"message": "An account already exists with that email."})
+  if (user) return res.status(409).json({"message": "An account already exists with that email."})
 
-
+  // Create new user object and hashes the password before save on line 35
   user = new UserModel(_.pick(req.body, ['firstName',
                                     'lastName',
                                     'email',
@@ -37,10 +40,9 @@ async function register(req, res, next) {
   res.locals.message = `User ${user.email} successfully created.`
 
   next()
-
-  // res.status(201).json({"message":`User ${user.email} successfully created.`, "token": token})
 }
 
+// Update User Details Route Logic
 async function updateDetails(req, res, next) {
 
   let validUser = await UserModel.findById(req.user._id)
@@ -64,20 +66,21 @@ async function updateDetails(req, res, next) {
 
 }
 
+// Update User Password Route Logic
 async function updatePassword(req, res, next) {
-
+  // Tries to grab user with req Id and return 404 if no valid user found
   let validUser = await UserModel.findById(req.user._id)
   .catch( (err) => { return res.status(404).json({ "message": err.message })})
-
+  // Validates password attached to user and return 400 if invalid
   const { error } = validatePassword({password: req.body.newPassword})
   if (error) return res.status(400).json({"message": 'Invalid password.'})
-
+  // Compares password provided to stored password
   const validPassword = await bcrypt.compare(req.body.currentPassword, validUser.password)
   if (!validPassword) return res.status(401).json({"message":"Invalid Email or Password."})
-
+  // Hashes new password
   const salt = await bcrypt.genSalt(10)
   validUser.password = await bcrypt.hash(req.body.newPassword, salt)
-
+  // Saves new password
   await validUser.save()
   res.status(200)
   res.locals.validUser = validUser
@@ -86,12 +89,11 @@ async function updatePassword(req, res, next) {
   next()
 }
 
+// Update User Email Route Logic
 async function updateEmail(req, res, next) {
 
   let validUser = await UserModel.findById(req.user._id)
   .catch( (err) => { return res.status(404).json({ "message": err.message })})
-
-
 
   let error = validateEmail({email: req.body.email}).error
   if (error) return res.status(400).json({"message": 'Email address must be a valid email.'})
@@ -104,7 +106,6 @@ async function updateEmail(req, res, next) {
 
   let user = await UserModel.findOne({ email: req.body.email })
   if(user) return res.status(409).json({"message": "An account already exists with that email."})
-
 
   validUser.email = req.body.email
 
